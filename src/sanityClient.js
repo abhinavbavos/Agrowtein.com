3688960e9ed543b45317a7757181b2e1245a381f
abhinav bavos <s4abhinav.19@gmail.com>
2024-07-05 11:48:18 +0530

    Initial

--- src/sanityClient.js
+++
@@ -1,8 +0,0 @@
-// src/sanityClient.js
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-  projectId: '3zg23lp8', // Replace with your project ID
-  dataset: 'blog', // Replace with your dataset name
-  useCdn: false, // `false` if you want to ensure fresh data
-});
